2972b69370c116ecbefc0ad29db1020c0956f482
YevhenMartyn <martyn.yevhen@gmail.com>
2024-05-22 18:30:31 +0200

    merge

--- Medium/Two_Sum_II.cs
+++ Medium/Two_Sum_II.cs
@@ -1,8 +1,34 @@
 ﻿using System;
 
-public class Class1
+
+public class Solution
 {
-	public Class1()
-	{
-	}
+    public int[] TwoSum(int[] nums, int target)
+    {
+
+        int leftBorder = 0;
+        int rightBorder = nums.Length - 1;
+
+        while (leftBorder < rightBorder)
+        {
+            if (nums[leftBorder] + nums[rightBorder] > target)
+            {
+                rightBorder--;
+            }
+            else if (nums[leftBorder] + nums[rightBorder] < target)
+            {
+                leftBorder++;
+            }
+            else if (nums[leftBorder] + nums[rightBorder] == target)
+            {
+                break;
+            }
+        }
+
+        int[] result = new int[2];
+        result[0] = ++leftBorder;
+        result[1] = ++rightBorder;
+        return result;
+    }
 }
+
